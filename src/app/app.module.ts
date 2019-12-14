c0492d1310a4f3c1fa8705b65e28bd87343bd7ad
WillyWork <dosananim@mail.ru>
2019-12-15 03:55:51 +1000

    add negative-vert-chart ver.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { ConsoleService } from './d3/test.service';
-import { NegativeChartComponent } from './components/negative-chart/negative-chart.component'
+import { NegativeChartComponent } from './components/negative-chart/negative-chart.component';
+import { NegativeVerticalChartComponent } from './components/negative-vertical-chart/negative-vertical-chart.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NegativeChartComponent
+    NegativeChartComponent,
+    NegativeVerticalChartComponent
   ],
   imports: [
     BrowserModule,
